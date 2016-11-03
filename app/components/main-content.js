4812a726bf7617e5efd17b6564735ae45582eef9
Zach Bergson <zach.bergson@gmail.com>
2016-11-03 14:11:10 -0400

    Main headline slideshow complete
    Will remove unnecessary addons later

--- app/components/main-content.js
+++ app/components/main-content.js
@@ -1,9 +1,42 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+const {
+  Component,
+  get,
+  run,
+  set,
+  testing
+} = Ember;
+
+export default Component.extend({
 
   defaultRegion: 'Africa',
   defaultLanguage: 'English',
+
+  regionCycle: ['Africa', 'Asia', 'Brussels Real Time', 'Canada', 'China', 'China Real Time', 'Economy', 'Europe', 'India Real Time', 'Latin America', 'Middle East'],
+
+  init() {
+    this._super(...arguments);
+    // Don't run the cycle during tests
+    if (!testing) { this.startCycle(); }
+  },
+
+  startCycle() {
+    let classCycle = get(this, 'regionCycle'),
+    currentClass = get(this, 'defaultRegion'),
+    currentIndex = classCycle.indexOf(currentClass);
+
+    if (currentIndex === (classCycle.length - 1)) {
+      set(this, 'defaultRegion', classCycle[0]);
+    } else {
+      set(this, 'defaultRegion', classCycle[currentIndex + 1]);
+    }
+    this.scheduleCycle();
+  },
+
+  scheduleCycle() {
+    set(this, 'timer', run.later(this, this.startCycle, 6000));
+  },
 
   actions: {
     changeRegion(region) {
